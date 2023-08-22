5b4a6f73f3308a7e67b153a594bf194186ffe3e6
Crowdbotics <team@crowdbotics.com>
2023-08-22 02:52:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-disk-43013",
+  url: "https://lively-disk-43013.botics.co",
+  api: "https://lively-disk-43013.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
